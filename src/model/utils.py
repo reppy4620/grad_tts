12cff920e23c308e72f2f7e89e1a5bf66fab2404
reppy4620 <ilikeniku@icloud.com>
2023-03-12 18:43:18 +0900

    delete unnecessary functions

--- src/model/utils.py
+++ src/model/utils.py
@@ -14,16 +14,6 @@ def convert_pad_shape(pad_shape):
     return pad_shape
 
 
-def init_weights(m, mean=0.0, std=0.01):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        m.weight.data.normal_(mean, std)
-
-
-def get_padding(kernel_size, dilation=1):
-    return int((kernel_size*dilation - dilation)/2)
-
-
 def generate_path(duration, mask):
     device = duration.device
     b, t_x, t_y = mask.shape
